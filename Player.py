1f429a535a021fdf1a9f5b57b8110271e1155fa4
Joseph <jchale247@gmail.com>
2023-10-30 10:21:46 -0500

    added main loop

--- Player.py
+++ Player.py
@@ -1,9 +1,13 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope='user-read-currently-playing streaming user-modify-playback-state user-read-playback-state',client_id ='98b2d6544c44499b8bcfa1d1b95ac160', client_secret = 'f5071da8a3124517888440cfbf78e32e',redirect_uri='https://records.jchomeserver.com/callback'))
+currentAlbum = ""
 
-client_credentials_manager = SpotifyClientCredentials(client_id ='98b2d6544c44499b8bcfa1d1b95ac160', client_secret = 'f5071da8a3124517888440cfbf78e32e')
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+while 1 == 1:
+    print(sp.devices())
+    nextAlbum = input("Album URL: ")
+    if currentAlbum != nextAlbum:
+        sp.start_playback('EDA5D75C-DDFC-4BA8-8A4D-D4E5986DDC1E', nextAlbum)
+        currentAlbum = nextAlbum 
 
-sp.start_playback('39a80c53edbd60f116ffb6e75d8bb994ac3c77e9', 'https://open.spotify.com/album/2yI4m5Yu2tl8v0It5P9WVz')
-
-'d59d5c2b-fec0-5ce1-bb61-e8896df130d0'
+#'d59d5c2b-fec0-5ce1-bb61-e8896df130d0'
