9a0c0e5d10f1b590f66ea5e28673d86de24da015
Joseph <jchale247@gmail.com>
2023-11-01 12:00:57 -0500

    added main loop

--- Player.py
+++ Player.py
@@ -1,13 +1,22 @@
 import spotipy
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 from spotipy.oauth2 import SpotifyOAuth
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope='user-read-currently-playing streaming user-modify-playback-state user-read-playback-state',client_id ='98b2d6544c44499b8bcfa1d1b95ac160', client_secret = 'f5071da8a3124517888440cfbf78e32e',redirect_uri='https://records.jchomeserver.com/callback'))
 currentAlbum = ""
+reader = SimpleMFRC522()
 
 while 1 == 1:
-    print(sp.devices())
-    nextAlbum = input("Album URL: ")
-    if currentAlbum != nextAlbum:
-        sp.start_playback('EDA5D75C-DDFC-4BA8-8A4D-D4E5986DDC1E', nextAlbum)
-        currentAlbum = nextAlbum 
-
-#'d59d5c2b-fec0-5ce1-bb61-e8896df130d0'
+    #print(sp.devices())
+    try:
+        id, text = reader.read()
+        nextAlbum = text
+        if currentAlbum != nextAlbum:
+            try:
+                sp.start_playback('6b65de863672452bbf099ee1807619b057cc5cac', nextAlbum)
+            except Exception:
+                print("invalid album")
+                continue
+            currentAlbum = nextAlbum 
+    except:
+        continue
